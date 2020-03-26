9d3de69e05f6c993f12b0c6f32891d1a8f1e17dc
Muktesharyan <58796106+Muktesharyan@users.noreply.github.com>
2020-03-26 12:28:41 +0530

    Adding quiz

---
+++ quiz/question.js
@@ -0,0 +1,93 @@
+
+function Quiz(questions) {
+    this.score = 0;
+    this.questions = questions;
+    this.questionIndex = 0;
+}
+
+Quiz.prototype.getQuestionIndex = function() {
+    return this.questions[this.questionIndex];
+}
+
+Quiz.prototype.guess = function(answer) {
+    if(this.getQuestionIndex().isCorrectAnswer(answer)) {
+        this.score++;
+    }
+
+    this.questionIndex++;
+}
+
+Quiz.prototype.isEnded = function() {
+    return this.questionIndex === this.questions.length;
+}
+
+
+function Question(text, choices, answer) {
+    this.text = text;
+    this.choices = choices;
+    this.answer = answer;
+}
+
+Question.prototype.isCorrectAnswer = function(choice) {
+    return this.answer === choice;
+}
+
+
+function populate() {
+    if(quiz.isEnded()) {
+        showScores();
+    }
+    else {
+        // show question
+        var element = document.getElementById("question");
+        element.innerHTML = quiz.getQuestionIndex().text;
+
+        // show options
+        var choices = quiz.getQuestionIndex().choices;
+        for(var i = 0; i < choices.length; i++) {
+            var element = document.getElementById("choice" + i);
+            element.innerHTML = choices[i];
+            guess("btn" + i, choices[i]);
+        }
+
+        showProgress();
+    }
+};
+
+function guess(id, guess) {
+    var button = document.getElementById(id);
+    button.onclick = function() {
+        quiz.guess(guess);
+        populate();
+    }
+};
+
+
+function showProgress() {
+    var currentQuestionNumber = quiz.questionIndex + 1;
+    var element = document.getElementById("progress");
+    element.innerHTML = "Question " + currentQuestionNumber + " of " + quiz.questions.length;
+};
+
+function showScores() {
+    var gameOverHTML = "<h1>Result</h1>";
+    gameOverHTML += "<h2 id='score'> Your score: " + quiz.score + "</h2>";
+
+    var element = document.getElementById("quiz");
+    element.innerHTML = gameOverHTML;
+};
+
+// create questions here
+var questions = [
+    new Question("Is there a vaccine, drug or treatment for COVID-19?", ["Yes", "Not yet"], "Not yet"),
+    new Question("Should you wear a mask always even if you dont have any symptoms?", [ "Yes", "No"], "No"),
+    new Question("Can COVID-19 be transmitted in areas with hot and humid climates?", [ "Yes", "No"], "Yes"),
+    new Question("Can mosquito bites transmit the new coronavirus?", [ "Yes", "No"], "No"),
+    new Question("Only old people are affected by the new coronavirus", [ "Yes", "No"], "No")
+];
+
+// create quiz
+var quiz = new Quiz(questions);
+
+// display quiz
+populate();
